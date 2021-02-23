488d5b50da85d7b8511167f6e8ccbfa8b411a1ea
Cara Wong <caraalyssa.wong@gmail.com>
2021-02-22 21:30:08 -0800

    create placeholder screen to test navigation

---
+++ src/screens/signup.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet, Test } from "react-native";
+import { mainColors } from "../constants/colours/appColors"
+import { MainButton } from "../components/button";
+
+export function SignUp(props) {
+    return (
+    <View>
+        <Text style={styles.font}>Test</Text>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    font: {
+        fontSize: 30,
+    }
+});
