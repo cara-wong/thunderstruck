b4d8870cc0c4bed53e4274021d937c8cc573b7d7
Cara Wong <caraalyssa.wong@gmail.com>
2021-03-21 17:50:12 -0700

    fix appdelgate-firebase connection

--- ios/thunderstruck/AppDelegate.m
+++ ios/thunderstruck/AppDelegate.m
@@ -12,8 +12,7 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
-@import UIKit;
-@import Firebase;
+#import <Firebase.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -30,7 +29,9 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [FIRApp configure];
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
