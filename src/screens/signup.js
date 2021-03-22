2977d795d7da8ba24c08fd2bdeadc5f43eeb2d42
Cara Wong <caraalyssa.wong@gmail.com>
2021-03-22 13:36:33 -0700

    implement signup to create a new account

--- src/screens/signup.js
+++ src/screens/signup.js
@@ -1,14 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text, TextInput } from "react-native";
 import CheckBox from '@react-native-community/checkbox';
+import auth from '@react-native-firebase/auth';
 
 import { MainButton } from "../components/button";
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, HeaderStyleInterpolators } from '@react-navigation/stack';
 
 
 
 export function SignUp(props) {
+
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+    const [isEighteen, setCheckBox] = useState(false);
+
+    isStrongPassword = () => {
+        return ((password.length > 8) && (password.match(/[a-z]/) !== null)
+        && (password.match(/[A-Z]/) !== null)
+        && (password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/) !== null));
+    };
+
+    async function hasValidInfo() {
+        if (!name || !email || !password || !confirmPassword) {
+            alert("Please fill in all fields to create an account");
+            return false;
+        }
+
+        if (password !== confirmPassword) {
+            alert("Your passwords do not match");
+            return false;
+        }
+
+        if (!this.isStrongPassword()) {
+            alert("Your password must be at least 8 characters and contain an uppercase, lowercase, and special characte");
+            return false;
+        }
+
+        if (!isEighteen) {
+            alert("You must be 18 years or older to use Thunderstruck");
+            return false;
+        }
+
+        await setEmail(email.toLowerCase());
+        
+        if (email.indexOf("@student.ubc.ca") === -1) {
+            alert("Please enter a valid UBC email");
+            return false;
+        }
+
+        setName(name.trim());
+        return true;
+    };
+
+    async function createAccount() {
+        if (await hasValidInfo()) {
+            let response = await auth().createUserWithEmailAndPassword(email, password)
+            .then(() => {
+              console.log('User account created & signed in!');
+              // todo: send verification email
+              props.navigation.navigate('CreateProfile');
+            })
+            .catch(error => {
+              if (error.code === 'auth/email-already-in-use') {
+                alert('That email address is already in use!');
+                return;
+              }
+          
+              if (error.code === 'auth/invalid-email') {
+                console.log('That email address is invalid!');
+                return;
+              }
+
+              if (error.code === 'auth/invalid-email') {
+                console.log('That email address is invalid!');
+                return;
+              }
+          
+              console.error(error);
+              alert("Something went wrong, please try again");
+            });
+        }
+    };
+
     return (
     <View style={styles.container}>
         <View style={styles.header}>
@@ -16,28 +92,25 @@ export function SignUp(props) {
         </View>
         <View style={styles.body}>
             <Text style={styles.body}>Name</Text>
-            <TextInput style = {styles.input}></TextInput>
+            <TextInput style={styles.input} onChangeText={(text) => setName(text)}></TextInput>
 
 
             <Text style={styles.body}>UBC Email</Text>
-            <TextInput style = {styles.input}></TextInput>
-             <Text style={styles.body}>Password</Text>
-             <TextInput style = {styles.input}></TextInput>
+            <TextInput style={styles.input} onChangeText={(email) => setEmail(email)}></TextInput>
 
+            <Text style={styles.body}>Password</Text>
+            <TextInput style={styles.input} onChangeText={(password) => setPassword(password)}></TextInput>
 
             <Text style={styles.body}>Re-Type Password</Text>
-            <TextInput style = {styles.input}></TextInput>
+            <TextInput style={styles.input} onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}></TextInput>
 
-            <CheckBox onCheckColor={'#FFCB37'} onTintColor={'#FFCB37'} style={styles.checkbox}></CheckBox>
+            <CheckBox value={isEighteen} onValueChange={setCheckBox} onCheckColor={'#FFCB37'} onTintColor={'#FFCB37'} style={styles.checkbox}></CheckBox>
             <Text style={styles.boxText} >Please verify that you are over 18 years of age</Text>
 
         </View>
         <View style={styles.button}>
-            <MainButton style={styles.button} title='Submit' onPress={() => props.navigation.navigate('CreateProfile')}/>
-
-        </View>
-            
-        
+            <MainButton style={styles.button} title='Submit' onPress={() => createAccount()}/>
+        </View> 
     </View>
     )
 }
